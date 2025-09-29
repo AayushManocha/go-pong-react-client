ef1b68d3726d1701fba6c77215529a755f682188
aayushmanocha <aayush.manocha@gmail.com>
2025-09-29 15:53:54 -0400

    improve smoothness of arrow controls

--- app/hooks/useGameControls.ts
+++ app/hooks/useGameControls.ts
@@ -1,7 +1,36 @@
 import { useHotkeys } from "@mantine/hooks";
+import { useEffect, useState } from "react";
 import { movePlayer } from "~/utils/api";
 
 export default function useGameControls(gameId: string) {
-  useHotkeys([["ArrowUp", () => movePlayer(gameId, "UP")]]);
-  useHotkeys([["ArrowDown", () => movePlayer(gameId, "DOWN")]]);
+  const [isMoving, setIsMoving] = useState(null);
+
+  useEffect(() => {
+    console.log("useGameControls");
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowUp") {
+        setIsMoving("UP");
+      } else if (e.key === "ArrowDown") {
+        setIsMoving("DOWN");
+      } else {
+        setIsMoving(null);
+      }
+    });
+
+    document.addEventListener("keyup", () => setIsMoving(null));
+  }, []);
+
+  useEffect(() => {
+    const handleMove = async (direction: string | null) => {
+      if (!direction) return;
+      await new Promise(() =>
+        setTimeout(() => movePlayer(gameId, direction), 10),
+      );
+    };
+
+    handleMove(isMoving);
+  });
+
+  // useHotkeys([["ArrowUp", () => movePlayer(gameId, "UP")]]);
+  // useHotkeys([["ArrowDown", () => movePlayer(gameId, "DOWN")]]);
 }
